e98536417d5afaa1745ca422263cddf27d1c508b
marius <dmunteanu91@yahoo.com>
2019-06-03 22:13:06 +0200

    rearranged code

--- app/src/main/java/com/example/keabank/HomeActivity.java
+++ app/src/main/java/com/example/keabank/HomeActivity.java
@@ -31,6 +31,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         mCustomer = getDebugCustomer();
 
         mAccountsList = findViewById(R.id.list_accounts);
