c7ebe2694c2a3c5280c8c705ecb231a0a6ab129b
marius <dmunteanu91@yahoo.com>
2019-06-03 09:45:14 +0200

    Revert "added java 1.8 dependent default method body"
    This reverts commit 1d3a7469

--- app/src/main/java/com/example/keabank/model/TransactionTarget.java
+++ app/src/main/java/com/example/keabank/model/TransactionTarget.java
@@ -5,15 +5,6 @@ public interface TransactionTarget {
 
     boolean canSubtractAmount(float amount);
 
-    // Save in case i manage to change language level
-//    default boolean canSubtractAmount(float amount) {
-//        if (canGoNegative()) {
-//            return true;
-//        } else {
-//            return getAmount() - amount > 0;
-//        }
-//    }
-
     void subtract(float amount);
 
     void increase(float amount);
