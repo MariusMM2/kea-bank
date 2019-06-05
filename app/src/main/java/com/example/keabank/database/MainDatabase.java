00c0cb5653977f84798a3307068cf84f856c6f8e
marius <dmunteanu91@yahoo.com>
2019-06-05 15:59:18 +0200

    moved dummy customer to MainDatabase

--- app/src/main/java/com/example/keabank/database/MainDatabase.java
+++ app/src/main/java/com/example/keabank/database/MainDatabase.java
@@ -4,8 +4,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import com.example.keabank.model.*;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @SuppressWarnings({"SpellCheckingInspection", "unchecked"})
@@ -29,6 +28,55 @@ public class MainDatabase {
         }
 
         return sInstance;
+    }
+
+    public static Customer getDummyCustomer() {
+        Customer customer = new Customer("John", "Doe", "johndoe@email.com", "123456", Calendar.getInstance().getTime());
+        final List<Account> accountList = new ArrayList<>(
+                Arrays.asList(
+                        Account.newDefault(3000, customer.getId()),
+                        Account.newBudget(1000, customer.getId())
+                )
+        );
+        final List<Bill> billList = new ArrayList<>(
+                Arrays.asList(
+                        new Bill("Bill 1", "1++", false, 10, Calendar.getInstance().getTime(), customer.getId()),
+                        new Bill("Bill 2", "2++", false, 20, Calendar.getInstance().getTime(), customer.getId()),
+                        new Bill("Bill 3", "3++", false, 30, Calendar.getInstance().getTime(), customer.getId())
+                )
+        );
+        final List<Transaction> transactionList = new ArrayList<>(
+                Arrays.asList(
+                        Transaction.beginTransaction().setSource(accountList.get(0)).setDestination(accountList.get(1)).setAmount(1000),
+                        Transaction.beginTransaction().setSource(accountList.get(1)).setDestination(accountList.get(0)).setAmount(2000),
+                        Transaction.beginTransaction().setSource(accountList.get(0)).setDestination(billList.get(0)).setAmount(billList.get(0).getAmount()),
+                        Transaction.beginTransaction().setSource(accountList.get(1)).setDestination(billList.get(2)).setAmount(billList.get(2).getAmount()),
+                        Transaction.beginTransaction().setSource(billList.get(1)).setDestination(accountList.get(0)).setAmount(billList.get(1).getAmount())
+                )
+        );
+
+        accountList.forEach(account -> {
+            List<Transaction> outGoingTransactions = transactionList.stream()
+                    .filter(transaction -> transaction.getSource().getId().equals(account.getId()))
+                    .collect(Collectors.toList());
+
+            List<Transaction> incomingTransactions = transactionList.stream()
+                    .filter(transaction -> transaction.getDestination().getId().equals(account.getId()))
+                    .map(Transaction::reverse)
+                    .collect(Collectors.toList());
+
+            List<Transaction> allTransactions = new ArrayList<>();
+
+            allTransactions.addAll(outGoingTransactions);
+            allTransactions.addAll(incomingTransactions);
+            allTransactions.sort(Comparator.comparing(Transaction::getDate));
+            Collections.reverse(allTransactions);
+            allTransactions.forEach(account::addTransaction);
+
+            customer.addAccount(account);
+        });
+
+        return customer;
     }
 
     public boolean tryLogin(NemId nemId) {
