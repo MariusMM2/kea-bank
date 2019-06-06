f034d398f982e0ed967589f25ced404942970681
marius <dmunteanu91@yahoo.com>
2019-06-07 00:36:06 +0200

    added logging

--- app/src/main/java/com/example/keabank/util/StringWrapper.java
+++ app/src/main/java/com/example/keabank/util/StringWrapper.java
@@ -2,16 +2,19 @@ package com.example.keabank.util;
 
 import android.content.Context;
 import android.text.format.DateFormat;
+import android.util.Log;
 import com.example.keabank.R;
 
 import java.util.Date;
 
 public class StringWrapper {
+    private static final String TAG = "StringWrapper";
     private static Context sContext;
 
     public static void init(Context context) {
         if (sContext == null) {
             sContext = context;
+            Log.d(TAG, "init: added Context reference");
         }
     }
 
