897e27fb2228db435760010e07b588e8c7d81ef6
marius <dmunteanu91@yahoo.com>
2019-06-08 22:03:31 +0200

    added random numerical string generator

--- app/src/main/java/exam/marius/keabank/util/StringUtils.java
+++ app/src/main/java/exam/marius/keabank/util/StringUtils.java
@@ -6,10 +6,12 @@ import android.util.Log;
 import exam.marius.keabank.R;
 
 import java.util.Date;
+import java.util.Random;
 
 public class StringUtils {
     private static final String TAG = "StringUtils";
     private static Context sContext;
+    private static Random sRandom = new Random();
 
     public static void init(Context context) {
         if (sContext == null) {
@@ -32,5 +34,12 @@ public class StringUtils {
         }
 
         return charSequence;
+    }
+
+    public static String getRandomNumerical(int digitCount) {
+        long l1 = sRandom.nextLong();
+        long l2 = l1 < 0 ? l1 + Long.MAX_VALUE : l1;
+
+        return String.valueOf(l2).substring(0, digitCount);
     }
 }
