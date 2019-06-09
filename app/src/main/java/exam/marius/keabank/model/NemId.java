448822e546bb8d25329d6f380350723c2b0e739c
marius <dmunteanu91@yahoo.com>
2019-06-10 00:31:58 +0200

    added parcelable implementation

--- app/src/main/java/exam/marius/keabank/model/NemId.java
+++ app/src/main/java/exam/marius/keabank/model/NemId.java
@@ -1,9 +1,13 @@
 package exam.marius.keabank.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+import exam.marius.keabank.util.ParcelUtils;
+
 import java.util.Objects;
 import java.util.UUID;
 
-public class NemId implements DatabaseItem {
+public class NemId implements DatabaseItem, Parcelable {
     private UUID mId;
     private String mUsername, mPassword;
     private UUID mCustomerId;
@@ -54,4 +58,36 @@ public class NemId implements DatabaseItem {
     public void setCustomerId(UUID customerId) {
         mCustomerId = customerId;
     }
+
+    protected NemId(Parcel in) {
+        ParcelUtils.readUuid(in);
+        mUsername = in.readString();
+        mPassword = in.readString();
+        ParcelUtils.readUuid(in);
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        ParcelUtils.writeUuid(dest, mId);
+        dest.writeString(mUsername);
+        dest.writeString(mPassword);
+        ParcelUtils.writeUuid(dest, mCustomerId);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<NemId> CREATOR = new Creator<NemId>() {
+        @Override
+        public NemId createFromParcel(Parcel in) {
+            return new NemId(in);
+        }
+
+        @Override
+        public NemId[] newArray(int size) {
+            return new NemId[size];
+        }
+    };
 }
