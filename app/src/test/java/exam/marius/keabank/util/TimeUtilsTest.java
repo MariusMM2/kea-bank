adadf1766d1265974ae334d41d535e488c545e2d
marius <dmunteanu91@yahoo.com>
2019-06-09 16:34:15 +0200

    updated unit testing

--- app/src/test/java/exam/marius/keabank/util/TimeUtilsTest.java
+++ app/src/test/java/exam/marius/keabank/util/TimeUtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
@@ -12,20 +13,89 @@ public class TimeUtilsTest {
 
     @Test
     public void weeksLeft() {
-        LocalDate d1 = LocalDate.parse("2019-04-10");
-        LocalDate d2 = LocalDate.parse("2019-04-30");
-        long weeksLeft = TimeUtils.weeksLeft(getDate(d1), getDate(d2));
-        assertEquals(2, weeksLeft);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 10);
+        Date date = calendar.getTime();
 
-        d1 = LocalDate.parse("2019-04-24");
-        d2 = LocalDate.parse("2019-04-30");
-        weeksLeft = TimeUtils.weeksLeft(getDate(d1), getDate(d2));
-        assertEquals(0, weeksLeft);
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 30);
+        Date date2 = calendar.getTime();
 
-        d1 = LocalDate.parse("2019-04-23");
-        d2 = LocalDate.parse("2019-04-30");
-        weeksLeft = TimeUtils.weeksLeft(getDate(d1), getDate(d2));
-        assertEquals(1, weeksLeft);
+        assertEquals(2, TimeUtils.weeksLeft(date, date2));
+
+        //
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 24);
+        date = calendar.getTime();
+
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 30);
+        date2 = calendar.getTime();
+
+        assertEquals(0, TimeUtils.weeksLeft(date, date2));
+
+        //
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 23);
+        date = calendar.getTime();
+
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 30);
+        date2 = calendar.getTime();
+
+        assertEquals(1, TimeUtils.weeksLeft(date, date2));
+    }
+
+    @Test
+    public void daysLeft() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 7);
+        Date date = calendar.getTime();
+
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 7);
+        Date date2 = calendar.getTime();
+
+        assertEquals(0, TimeUtils.daysLeft(date, date2));
+
+        //
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 7);
+        date = calendar.getTime();
+
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 8);
+        date2 = calendar.getTime();
+
+        assertEquals(1, TimeUtils.daysLeft(date, date2));
+
+        //
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 7);
+        date = calendar.getTime();
+
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 6, 9);
+        date2 = calendar.getTime();
+
+        assertEquals(2, TimeUtils.daysLeft(date, date2));
+
+        //
+        calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        date = calendar.getTime();
+        System.out.println(date);
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 5, 10);
+        date2 = calendar.getTime();
+        System.out.println(date2);
+
+        assertEquals(1, TimeUtils.daysLeft(date, date2));
+    }
+
+    @Test
+    public void asd() {
+        System.out.println(TimeUtils.getToday());
     }
 
     private Date getDate(LocalDate localDate) {
